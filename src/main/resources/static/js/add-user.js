64c50cbbef06b03efee117e9f385b38bc7d798f6
duongnc <duongnc@rikkeisoft.com>
2024-01-12 08:57:40 +0900

    commit

--- src/main/resources/static/js/add-user.js
+++ src/main/resources/static/js/add-user.js
@@ -3,9 +3,9 @@ const passwordInput = document.getElementById("password")
 const emailError = document.getElementById("emailErr")
 const passwordError = document.getElementById("passwordErr")
 
-emailInput.addEventListener("keyup", () => {
+emailInput.addEventListener("keyup", function () {
     emailError.style.display = "none"
 })
-passwordInput.addEventListener("keyup", () => {
+passwordInput.addEventListener("keyup", function () {
     passwordError.style.display = "none"
 })
